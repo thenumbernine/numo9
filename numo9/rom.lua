8115a0b6cc7c736b3248d5d93df8d9b5e2a14b6f
Christopher Moore <christopher.e.moore@gmail.com>
2024-09-29 12:01:06 -0700

    more on audio

--- numo9/rom.lua
+++ numo9/rom.lua
@@ -21,9 +21,16 @@ local tilemapSize = vec2i(256, 256)
 local tilemapSizeInSprites = vec2i(tilemapSize.x /  spriteSize.x, tilemapSize.y /  spriteSize.y)
 local codeSize = 0x10000	-- tic80's size ... but with my langfix shorthands like pico8 has
 
-local sfxTableSize =  256	-- max number of sounds played at a time
-local sfxDataSize = 0x8000	-- snes had 64k dedicated to audio so :shrug:
-local numChannels = 8
+--local audioSampleType = 'uint8_t'
+local audioSampleType = 'int16_t'
+--local audioSampleRate = 22050
+local audioSampleRate = 32000
+--local audioSampleRate = 44100
+local audioMixChannels = 8	-- # channels to play at the same time
+local audioOutChannels = 2	-- 1 for mono, 2 for stereo ... # L/R samples-per-sample-frame ... there's so much conflated terms in audio programming ...
+local sfxTableSize =  256	-- max number of unique sfx that a music can reference
+local musicTableSize = 256	-- max number of music tracks stored
+local audioDataSize = 0x10000	-- snes had 64k dedicated to audio so :shrug:
 
 --local fontWidth = spriteSize.x
 local fontWidth = 5
@@ -57,7 +64,14 @@ local mvMatType = 'int16_t'
 local mvMatScale = 256
 --]]
 
-
+-- instead of a 'length' i could store an 'end-addr'
+local AddrLen = struct{
+	name = 'AddrLen',
+	fields = {
+		{name='addr', type='uint16_t'},
+		{name='len', type='uint16_t'},
+	},
+}
 
 local ROM = struct{
 	name = 'ROM',
@@ -68,10 +82,55 @@ local ROM = struct{
 			anonymous = true,
 			packed = true,
 			fields = {
+				-- [[ video stuff
 				{name='spriteSheet', type='uint8_t['..spriteSheetSize:volume()..']'},
 				{name='tileSheet', type='uint8_t['..spriteSheetSize:volume()..']'},
 				{name='tilemap', type='uint16_t['..tilemapSize:volume()..']'},
 				{name='palette', type='uint16_t['..paletteSize..']'},
+				--]]
+
+				-- [[ audio stuff
+				-- sfxs should have -start addr -loop addr (what to play next ... any addr in audio ram)
+				-- so my sfx == pico8/tic80's waveforms
+				-- tempting to store this all with BRR... that will reduce the size by 32/9 ~ 3.555x
+				-- should I put the end-addr/length here, or should I store it as a first byte of the waveform data?
+				-- put here = more space, but leaves sequences of waveforms contiguous so we can point into the lump sum of all samples without worrying about dodging other data
+				-- put there = halves the space of this array.  if you want one track for hte whole of audio RAM then you only store one 'length' value.
+				{name='sfxAddrs', type='AddrLen['..sfxTableSize..']'},
+
+				-- playback information for sfx
+				-- so my music == pico8/tic80's sfx ... and rlly their music is just some small references to start loop / end loop of their sfx.
+				{name='musicAddrs', type='AddrLen['..musicTableSize..']'},
+
+				-- technically I should be cutting the addrs out of the 64kb
+				{name='audioData', type='uint8_t['..audioDataSize..']'},
+				--]]
+
+				{name='code', type='uint8_t['..codeSize..']'},
+			},
+		}},
+	},
+}
+--DEBUG:print(ROM.code)
+--DEBUG:print('ROM size', ffi.sizeof(ROM))
+
+
+--[[
+music format ...
+... sequence of commands at dif intervals
+- delta encoding of the channel state below:
+- plus delta time
+
+sequence of sfx notes and channels ... to be issued at various times ...
+i could create a definite 1/120 beat to issue them like pico8/tic80 ...
+... or i could just specify arbitrary offsets like spc lets you play things arbitrarily ...
+
+... how about a list of channels and channel-commands to issue with durations between them / between issuing them?
+same contents as a Numo9Channel has?
+then to reproduc fantasy consoles we could store the 8 waveforms in numo9's "sfx" data
+ 	and store the sfx and music data in numo9's "music" data
+--]]
+
 
 --[[
 waveforms ...
@@ -101,19 +160,6 @@ how about I first say audio is stored mono 16bit-samples ... any length?
 - echo volume L R
 - echo feedback ?
 --]]
-				{name='sfxAddrs', type='uint16_t['..sfxTableSize..']'},
-
-				-- TODO tempting to store this all with BRR...
-				{name='sfxData', type='uint16_t['..sfxDataSize..']'},
-
-				{name='code', type='uint8_t['..codeSize..']'},
-			},
-		}},
-	},
-}
---DEBUG:print(ROM.code)
---DEBUG:print('ROM size', ffi.sizeof(ROM))
-
 local Numo9Channel = struct{
 	name = 'Numo9Channel',
 	fields = {
@@ -134,7 +180,7 @@ local Numo9Channel = struct{
 local function maxrangeforsize(s) return bit.lshift(1, bit.lshift(s, 3)) end
 
 -- make sure our sfx table can address all our sound ram
---assert(maxrangeforsize(ffi.sizeof(RAM.sfxAddrs[0])) >= ffi.sizeof(RAM.sfxData))
+--assert(maxrangeforsize(ffi.sizeof(RAM.sfxAddrs[0])) >= ffi.sizeof(RAM.audioData))
 
 -- make sure we can index all our sfx in the table
 --assert(maxrangeforsize(ffi.sizeof(Numo9Channel.fields.sfxID.type)) >= sfxTableSize)
@@ -168,7 +214,7 @@ local RAM = struct{
 				{name='blendColor', type='uint16_t[1]'},
 
 				-- audio
-				{name='channels', type='Numo9Channel['..numChannels..']'},
+				{name='channels', type='Numo9Channel['..audioMixChannels..']'},
 
 				-- timer
 				{name='updateCounter', type='uint32_t[1]'},	-- how many updates() overall, i.e. system clock
@@ -230,6 +276,23 @@ local function unpackptr(n, p)
 	return p[0], unpackptr(n-1, p+1)
 end
 
+local function deltaCompress(
+	prevp,	-- previous state, of T*
+	nextp,	-- next state, of T*
+	len,	-- state length
+	dstvec	-- a vector'T' for now
+)
+	for i=0,len-1 do
+		if nextp[0] ~= prevp[0] then
+			dstvec:emplace_back()[0] = i
+			dstvec:emplace_back()[0] = nextp[0]
+		end
+		nextp=nextp+1
+		prevp=prevp+1
+	end
+end
+
+
 return {
 	paletteSize = paletteSize,
 	spriteSize = spriteSize,
@@ -245,6 +308,13 @@ return {
 	mvMatScale = mvMatScale,
 	keyPressFlagSize = keyPressFlagSize,
 	keyCount = keyCount,
+
+	audioSampleType = audioSampleType,
+	audioSampleRate = audioSampleRate,
+	audioMixChannels = audioMixChannels,
+	sfxTableSize = sfxTableSize,
+	audioDataSize = audioDataSize,
+
 	ROM = ROM,
 	RAM = RAM,
 
@@ -272,4 +342,5 @@ return {
 
 	packptr = packptr,
 	unpackptr = unpackptr,
+	deltaCompress = deltaCompress,
 }
