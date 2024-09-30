57e44140fc205bb4063b7640027db9441766e5d6
Christopher Moore <christopher.e.moore@gmail.com>
2024-09-30 16:03:43 -0700

    ok things are starting to work reasonably.  for some reason when unchecked, sdl would queue up 4s worth of audio, then the friendly consume/produce relation kicked in.  this wouldnt be a problem if I was using SDL_GetQueue... to check its size, but I avoided this at first when trying with larger queues because it returned in increments of the queue size

--- numo9/app.lua
+++ numo9/app.lua
@@ -984,7 +984,8 @@ function App:update()
 	if fpsSeconds > 1 then
 		print(
 		--	'FPS: '..(fpsFrames / fpsSeconds)	--	this will show you how fast a busy loop runs ... 130,000 hits/second on my machine ... should I throw in some kind of event to lighten the cpu load a bit?
-			'draws/second '..drawsPerSecond	-- TODO make this single-buffered
+		--	'draws/second '..drawsPerSecond	-- TODO make this single-buffered
+			'SDL_GetQueuedAudioSize', sdl.SDL_GetQueuedAudioSize(self.audio.deviceID)
 		)
 		if self.server then
 			--[[
@@ -1075,7 +1076,7 @@ conn.receivesPerSecond = 0
 	if needUpdateCounter > 0 then
 		-- TODO decrement to use framedrops
 		needUpdateCounter = 0
-		
+
 		-- [[ where should I even put this?
 		self:updateAudio()
 		--]]
