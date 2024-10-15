c3848501806bf89808ec38e0d89d82f418f702bb
Christopher Moore <christopher.e.moore@gmail.com>
2024-10-15 15:09:30 +0600

    changed cartridge to be a custom png chunk, added a default cartridge image, made the archiver generate pngs with custom chunks as cartridge files

--- numo9/archive.lua
+++ numo9/archive.lua
@@ -19,6 +19,7 @@ local assertle = require 'ext.assert'.le
 local asserteq = require 'ext.assert'.eq
 local assertge = require 'ext.assert'.ge
 local asserttype = require 'ext.assert'.type
+local assertindex = require 'ext.assert'.index
 local path = require 'ext.path'
 local vec3i = require 'vec-ffi.vec3i'
 local Image = require 'image'
@@ -42,22 +43,46 @@ end
 -- TODO image io is tied to file rw because so many image format libraries are also tied to file rw...
 -- so reading is from files now
 local tmploc = path'___tmp.png'
-
+local pngCustomKey = 'nuMO'
 --[[
 assumes 'rom' is ptr to the start of our ROM memory
 creates an Image and returns it
 --]]
 local function toCartImage(rom, labelImage)
 	asserttype(rom, 'cdata')
-	-- [[ saving the raw binary
+	--[[ saving the raw binary
 	return ffi.string(rom, ffi.sizeof'ROM')
 	--]]
 
-	--[=[
+	-- [=[ save as an image
 	-- TODO image hardcodes this to 1) file io and 2) extension ... because a lot of the underlying image format apis do too ... fix me plz
 	--[[ store it as an 8bpp PNG ...
 	local romImage = Image(cartImageSize.x, cartImageSize.y, cartImageSize.z, 'uint8_t'):clear()
 	ffi.copy(romImage.buffer, rom, ffi.sizeof'ROM')
+	--]]
+	-- [[ storing in png metadata
+	local baseLabelImage = Image'defaultlabel.png'
+	asserteq(baseLabelImage.channels, 4)
+	local romImage = Image(baseLabelImage.width, baseLabelImage.height, 4, 'uint8_t'):clear()
+	if labelImage then
+		romImage:pasteInto{image=labelImage, x=math.floor((romImage.width-labelImage.width)/2), y=0}
+	end
+	--[[
+	romImage:pasteInto{image=baseLabelImage, x=0, y=0}
+	--]]
+	-- [[ paste-with-alpha, TODO move to image library? 
+	for i=0,baseLabelImage.width*baseLabelImage.height-1 do
+		local dstp = romImage.buffer + 4 * i
+		local srcp = baseLabelImage.buffer + 4 * i
+		for ch=0,2 do
+			local f = srcp[3] / 255
+			dstp[ch] = srcp[ch] * f + dstp[ch] * (1 - f)
+		end
+		dstp[3] = math.max(srcp[3], dstp[3])
+	end
+	--]]
+	romImage.unknown = romImage.unknown or {}
+	romImage.unknown[pngCustomKey] = {data=ffi.string(rom, ffi.sizeof'ROM')}
 	--]]
 	--[[ if it's 16bpp ... use the lower byte for storage and upper byte for a cartridge label image
 	local romImage = Image(cartImageSize.x, cartImageSize.y, cartImageSize.z, 'uint16_t'):clear()
@@ -112,26 +137,30 @@ end
 takes an Image
 --]]
 local function fromCartImage(srcData)
-	-- [[ saving/loading raw data
 	local rom = ffi.new'ROM'
+	--[[ saving/loading raw data
 	ffi.copy(rom.v, ffi.cast('uint8_t*', srcData), ffi.sizeof'ROM')
 	return rom
 	--]]
-	--[=[ saving/loading image data
+	-- [=[ loading as an image
 --DEBUG:assert(not tmploc:exists())
 	assert(path(tmploc):write(srcData))
 	local romImage = Image(tmploc.path)
 	tmploc:remove()
 --DEBUG:assert(not tmploc:exists())
-
+	-- [[ storing in png metadata
+	local data = assertindex(romImage.unknown or {}, pngCustomKey, "couldn't find png custom chunk").data
+	assertle(ffi.sizeof'ROM', #data)
+	ffi.copy(rom.v, data, ffi.sizeof'ROM')
+	--]]
+	--[[ if it's 8bpp ...
 	asserteq(romImage.channels, 3)
 	assertle(ffi.sizeof'ROM', romImage.width * romImage.height * romImage.channels)
-
-	local rom = ffi.new'ROM'
-	-- [[ if it's 8bpp ...
 	ffi.copy(rom.v, romImage.buffer, ffi.sizeof'ROM')
 	--]]
 	--[[ if it's 16bpp ...
+	asserteq(romImage.channels, 3)
+	assertle(ffi.sizeof'ROM', romImage.width * romImage.height * romImage.channels)
 	local index = 0
 	for y=0,cartImageSize.y-1 do
 		for x=0,cartImageSize.x-1 do
