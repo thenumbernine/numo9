d2f74ca07fc01e101d0c78fb8eeccf386d25fca5
Christopher Moore <christopher.e.moore@gmail.com>
2024-10-20 15:35:49 -0700

    added -video for starting with init window size

--- run.lua
+++ run.lua
@@ -1,4 +1,6 @@
 #!/usr/bin/env luajit
+local assert = require 'ext.assert'
+local string = require 'ext.string'
 
 -- TODO cli here
 cmdline = {...}	-- global for later
@@ -8,6 +10,10 @@ for i=#cmdline-1,1,-1 do
 	if cmdline[i] == '-e' then
 		table.remove(cmdline, i)
 		cmdline.initCmd = table.remove(cmdline, i)
+	end
+	if cmdline[i] == '-video' then
+		table.remove(cmdline, i)
+		cmdline.video = assert.len(string.split(table.remove(cmdline, i), 'x'):mapi(function(x) return assert(tonumber(x)) end), 2)
 	end
 end
 
