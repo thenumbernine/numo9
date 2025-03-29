abfba2cf7cc4f2f75a7b59638148c8fee70d60d5
Christopher Moore <christopher.e.moore@gmail.com>
2025-03-29 23:05:48 +0800

    more

--- asteroids/code.lua
+++ asteroids/code.lua
@@ -41,8 +41,9 @@ vec2.init=[:,...]do
 		self.x, self.y = ...
 	end
 end
+vec2.set = [a,b] do a.x=b.x a.y=b.y return a end
 vec2.clone = [v] vec2(v.x, v.y)
-vec2.exp = [theta] vec2(math.cos(theta), math.sin(theta))
+vec2.unpack = [v] (v.x, v.y)
 vec2.__unm = [v] vec2(-v.x, -v.y)
 vec2.__add = [a,b] vec2(a.x+b.x, a.y+b.y)
 vec2.__sub = [a,b] vec2(a.x-b.x, a.y-b.y)
@@ -58,6 +59,7 @@ vec2.__mul = [a,b] do
 end
 vec2.__tostring = [v] '{'..tostring(v.x)..', '..tostring(v.y)..'}'
 vec2.__concat = string.concat
+vec2.exp = [theta] vec2(math.cos(theta), math.sin(theta))
 vec2.cross = [a,b] a.x * b.y - a.y * b.x
 vec2.dot = [a,b] a.x * b.x + a.y * b.y
 vec2.lenSq = [v] v:dot(v)
@@ -73,6 +75,103 @@ Quat.init=[:,...]do
 		self.x, self.y, self.z, self.w = ...
 	end
 end
+Quat.set = [:,o] do 
+	self.x, self.y, self.z, self.w = o:unpack() 
+	return self 
+end
+Quat.unpack = [q] (q.x, q.y, q.z, q.w)
+Quat.mul = [q, r, res] do
+	if not res then res = Quat() end
+	local a = (q.w + q.x) * (r.w + r.x)
+	local b = (q.z - q.y) * (r.y - r.z)
+	local c = (q.x - q.w) * (r.y + r.z)
+	local d = (q.y + q.z) * (r.x - r.w)
+	local e = (q.x + q.z) * (r.x + r.y)
+	local f = (q.x - q.z) * (r.x - r.y)
+	local g = (q.w + q.y) * (r.w - r.z)
+	local h = (q.w - q.y) * (r.w + r.z)
+	return res:set(
+		a - .5 * ( e + f + g + h),
+		-c + .5 * ( e - f + g - h),
+		-d + .5 * ( e - f - g + h),
+		b + .5 * (-e - f + g + h))
+end
+Quat.__mul = Quat.mul
+Quat.__div = [a,b] a * b:conjugate() / b:lenSq()
+
+Quat.epsilon = 1e-15
+Quat.toAngleAxis = [:, res] do
+	res = res or Quat()
+
+	local cosom = math.clamp(self.w, -1, 1)
+
+	local halfangle = math.acos(cosom)
+	local scale = math.sin(halfangle)
+
+	if scale >= -self.epsilon and scale <= self.epsilon then
+		return res:set(0,0,1,0)
+	end
+	scale = 1 / scale
+	return res:set(
+		self.x * scale,
+		self.y * scale,
+		self.z * scale,
+		halfangle * 2)
+end
+
+-- TODO epsilon-test this?  so no nans?
+Quat.fromAngleAxis = [:, res] do
+	local x, y, z, theta = self:unpack()
+	local vlen = math.sqrt(x*x + y*y + z*z)
+	local costh = math.cos(theta / 2)
+	local sinth = math.sin(theta / 2)
+	local vscale = sinth / vlen
+	return (res or Quat()):set(x * vscale, y * vscale, z * vscale, costh)
+end
+
+Quat.xAxis = [q, res] 
+	((res or vec3()):set(
+		1 - 2 * (q.y * q.y + q.z * q.z),
+		2 * (q.x * q.y + q.z * q.w),
+		2 * (q.x * q.z - q.w * q.y)))
+
+Quat.yAxis = [q, res]
+	((res or vec3()):set(
+		2 * (q.x * q.y - q.w * q.z),
+		1 - 2 * (q.x * q.x + q.z * q.z),
+		2 * (q.y * q.z + q.w * q.x)))
+
+Quat.zAxis = [q, res]
+		((res or vec3()):set(
+			2 * (q.x * q.z + q.w * q.y),
+			2 * (q.y * q.z - q.w * q.x),
+			1 - 2 * (q.x * q.x + q.y * q.y)))
+
+Quat.rotate = [:, v, res] do
+	local v4 = self * Quat(v.x, v.y, v.z, 0) * self:conjugate()
+	return (res or vec3()):set(v4.x, v4.y, v4.z)
+end
+
+Quat.conjugate = [:, res]
+	((res or Quat()):set(-self.x, -self.y, -self.z, self.w))
+
+Quat.normalize = [:, res, eps] do
+	eps = eps or Quat.epsilon
+	res = res or Quat()
+	local lenSq = self:normSq()
+	if lenSq < eps*eps then
+		return res:set(0,0,0,1)
+	end
+	local invlen = 1 / math.sqrt(lenSq)
+	return res:set(
+		self.x * invlen,
+		self.y * invlen,
+		self.z * invlen,
+		self.w * invlen)
+end
+
+
+worldSize = 256
 
 local dt = 1/60
 
@@ -84,6 +183,22 @@ drawTri = [pos, fwd, size, color] do
 end
 
 objs = table()
+
+getViewPos = [v] do
+	local relpos = v - viewPos
+	if relpos.x < -worldSize then 
+		relpos.x += 2*worldSize
+	elseif relpos.x > worldSize then 
+		relpos.x -= 2*worldSize
+	end
+	if relpos.y < -worldSize then 
+		relpos.y += 2*worldSize
+	elseif relpos.y > worldSize then 
+		relpos.y -= 2*worldSize
+	end
+	return relpos + viewPos
+end
+
 local Object = class()
 Object.pos = vec2()
 Object.vel = vec2()
@@ -105,12 +220,12 @@ Object.update=[:]do
 	self.pos += dt * self.vel
 
 	self.angle %= 2 * math.pi
-	self.pos.x += 128
-	self.pos.y += 128
-	self.pos.x %= 256
-	self.pos.y %= 256
-	self.pos.x -= 128
-	self.pos.y -= 128
+	self.pos.x += worldSize
+	self.pos.y += worldSize
+	self.pos.x %= 2*worldSize
+	self.pos.y %= 2*worldSize
+	self.pos.x -= worldSize
+	self.pos.y -= worldSize
 end
 
 local Shot = Object:subclass()
@@ -124,7 +239,7 @@ Shot.update=[:]do
 	Shot.super.update(self)
 
 	matpush()
-	mattrans(self.pos.x, self.pos.y)
+	mattrans(getViewPos(self.pos):unpack())
 	rect(-1, -1, 3, 3, self.color)
 	matpop()
 
@@ -168,7 +283,7 @@ Ship.nextShootTime = 0
 Ship.update=[:]do
 	local fwd = vec2.exp(self.angle)
 	matpush()
-	mattrans(self.pos.x, self.pos.y)
+	mattrans(getViewPos(self.pos):unpack())
 	drawTri(vec2(), fwd, self.size, self.color)
 
 	if self.thrust then
@@ -182,7 +297,7 @@ Ship.update=[:]do
 end
 Ship.shoot = [:]do
 	if self.nextShootTime > time() then return end
-	self.nextShootTime = time() + .3
+	self.nextShootTime = time() + .2
 	local fwd = vec2.exp(self.angle)
 	Shot{
 		pos = self.pos:clone(),
@@ -236,7 +351,7 @@ Rock.update=[:]do
 	local rightx = -fwd.y
 	local righty = fwd.x
 	matpush()
-	mattrans(self.pos.x, self.pos.y)
+	mattrans(getViewPos(self.pos):unpack())
 	line(self.size*(fwd.x+rightx), self.size*(fwd.y+righty), self.size*(rightx - fwd.x), self.size*(righty - fwd.y), self.color)
 	line(self.size*(rightx-fwd.x), self.size*(righty-fwd.y), self.size*(-rightx - fwd.x), self.size*(-righty - fwd.y), self.color)
 	line(self.size*(-rightx - fwd.x), self.size*(-righty - fwd.y), self.size*(fwd.x-rightx), self.size*(fwd.y-righty), self.color)
@@ -255,18 +370,29 @@ EnemyShip{
 --]]
 for i=1,5 do
 	Rock{
-		pos=vec2(math.random(), math.random())*256,
+		pos=vec2.exp(math.random()*2*math.pi) * worldSize * .5,
 		vel=vec2.exp(math.random()*2*math.pi) * 10,
 		angle = math.random() * 2 * math.pi,
 		rot = math.random() * 2 * 20,
 	}
 end
 
+viewPos = vec2()
+viewAngle = 0
 update=[]do
 	matident()	-- TODO cls() needs matident() manually set ...
 	cls()
 	matident()
-	mattrans(128, 128)
+	mattrans(128, 128)	-- screen center
+	matrot(-viewAngle)
+	mattrans(-viewPos.x, -viewPos.y)
+	if player then 
+		viewPos:set(player.pos)
+		viewAngle = player.angle + .5 * math.pi
+	end
+
+	-- screen boundary for debugging
+	rectb(-worldSize,-worldSize,2*worldSize,2*worldSize,1)
 
 	for i=#objs,1,-1 do
 		local o = objs[i]
