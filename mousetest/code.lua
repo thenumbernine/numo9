7aeb0fdd8eed312f40a4e6fdd37d590ccd33f352
Christopher Moore <christopher.e.moore@gmail.com>
2025-05-23 14:44:11 +0700

    more tests

---
+++ mousetest/code.lua
@@ -0,0 +1,13 @@
+local sx, sy = 1, 1
+update=[]do
+	local x, y, ssx, ssy = mouse()
+	sx += ssx
+	sy += ssy
+	local bl = key'mouse_left'
+	spr(bl and 0 or 1, 
+		x - 4, y - 4,
+		1, 1,
+		nil, nil,
+		nil, nil,
+		sx, sy)
+end
