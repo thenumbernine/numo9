f4ba0b515f9327044af879149a498954bbaf39c9
Christopher Moore <christopher.e.moore@gmail.com>
2026-06-04 17:58:46 +0700

    more

--- test-mode/code.lua
+++ test-mode/code.lua
@@ -1,4 +1,4 @@
---#include ext/range.lua
+local range = require 'ext.range'
 
 -- i could store resolution in the RAM, but nah, just rtfm
 local modes = range(0,49):append{255}
@@ -9,14 +9,14 @@ update=||do
 
 	local m = tonumber((t % #modes) // 1)
 	mode(modes[m+1])
-	local w, h = 
+	local w, h =
 		peekw(ramaddr'screenWidth'),
 		peekw(ramaddr'screenHeight')
 
-	local x1, y1 = 
+	local x1, y1 =
 		w * (math.cos(t * .4) * .5 + .5),
 		h * (math.sin(t * .5) * .5 + .5)
-	local x2, y2 = 
+	local x2, y2 =
 		w * (math.cos(t * .6) * .5 + .5),
 		h * (math.sin(t * .7) * .5 + .5)
 	local xmin = math.min(x1, x2)
@@ -31,7 +31,7 @@ update=||do
 
 	local xmid = .5 * (x1 + x2)
 	local ymid = .5 * (y1 + y2)
-	
+
 	local w, h = text('mode '..modes[m+1], -999, -999)
 	mattrans(xmid, ymid)
 	matrot(t)
