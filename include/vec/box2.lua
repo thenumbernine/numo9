519b40f612709297fa81192ebc636c7ddd3893d3
Christopher Moore <christopher.e.moore@gmail.com>
2026-06-11 12:10:16 +0700

    got rid of 1s

--- include/vec/box2.lua
+++ include/vec/box2.lua
@@ -66,16 +66,17 @@ box2=class{
 		end
 	end,
 	touches=|:,b|do
-		return self.min.x < b.max.x
-			and self.max.x > b.min.x
-			and self.min.y < b.max.y
-			and self.max.y > b.min.y
-	end,
-	touchesE=|:,b|do
 		return self.min.x <= b.max.x
 			and self.max.x >= b.min.x
 			and self.min.y <= b.max.y
 			and self.max.y >= b.min.y
+	end,
+	-- touches excluding boundary
+	touchesE=|:,b|do
+		return self.min.x < b.max.x
+			and self.max.x > b.min.x
+			and self.min.y < b.max.y
+			and self.max.y > b.min.y
 	end,
 	map=|b,c|c*b:size()+b.min,
 	__add=|a,b|box2(box2_getminvalue(a)+box2_getminvalue(b),box2_getmaxvalue(a)+box2_getmaxvalue(b)),
--- rectangle-number-game/code.lua
+++ rectangle-number-game/code.lua
@@ -4,49 +4,98 @@ range = require 'ext.range'
 vec2 = require 'vec.vec2'
 box2 = require 'vec.box2'
 
+--[[ beginner?
 boardSize = vec2(8, 8)
-boardBBox = box2(vec2(1,1), boardSize)
 tileSize = 24
+numPieces = 8
+--]]
+-- [[ expert?
+boardSize = vec2(20, 20)
+tileSize = 12
+numPieces = 64
+--]]
+
+boardBBox = box2(vec2(1,1), boardSize)
 
 -- ??fixme?
 red = 3
 green = 7
 
 Rect=class()
-Rect.init=|:,bbox|do
-	self.bbox = bbox:clone()
+Rect.init=|:,args|do
+	self.bbox = args!.bbox:clone()
+	self.color = args.color
+end
+Rect.clone=|:| Rect(self)
+Rect.size=|:| self.bbox:size()+1
+Rect.area=|:| self:size():product()
+Rect.draw=|:|do
+	local b = self.bbox
+	local x = tileSize * b.min.x + 2
+	local y = tileSize * b.min.y + 2
+	local w = tileSize * (b.max.x - b.min.x + 1) - 6
+	local h = tileSize * (b.max.y - b.min.y + 1) - 6
+	rect(x, y, w, h, self.color)
+	rectb(x, y, w, h, 12)
 end
 
--- get our soln - start with one giant rect
-solnRects = table{Rect(boardBBox)}
-local numPieces = 8
-for try=1,math.huge do
-	if #solnRects >= numPieces then break end
-	local r = solnRects:pickRandom()
-	--local i = vec2.fields:pickRandom()
-	local i = vec2.fields[(try%2)+1]
-	local rsize = r.bbox:size()+1	-- ... inclusive-size ...
-	if rsize[i] > 1 then
-		local r2 = Rect(r.bbox)
-		local mid = math.random(r.bbox.min[i], r.bbox.max[i]-1)
-		r.bbox.max[i] = mid
-		r2.bbox.min[i] = mid+1
-		solnRects:insert(r2)
+newGame=||do
+	-- get our soln - start with one giant rect
+	solnRects = table{Rect{bbox=boardBBox}}
+	for try=1,math.huge do
+		if #solnRects >= numPieces then break end
+
+		-- linear pick-random
+		--local r = solnRects:pickRandom()
+		-- quadratic pick-random to favor table start <-> larger boxes
+		local ri = 1 + math.floor(math.random() * math.random() * #solnRects)
+
+		local r = solnRects[ri]:clone()
+		local rsize = r:size()
+		--local i = vec2.fields:pickRandom()	-- random axis
+		--local i = vec2.fields[(try%2)+1]		-- alternating axis
+		-- weight axis by each dimension, so larger gets divided more often
+		local i = math.random() * (rsize.x + rsize.y) < rsize.x and 'x' or 'y'
+		if rsize[i] > 1 then
+			local r2 = r:clone()
+			-- hmm maybe todo weight random towards middle than ends?
+			local mid = math.random(r.bbox.min[i], r.bbox.max[i]-1)
+			r.bbox.max[i] = mid
+			r2.bbox.min[i] = mid+1
+			if r:area() > 1 and r2:area() > 1 then
+				-- out with old
+				solnRects:remove(ri)
+				-- insert new boxes
+				solnRects:insert(r)
+				solnRects:insert(r2)
+				-- sort boxes by area, largest to smallest
+				solnRects:sort(|a,b| a:area() > b:area())
+			end
+		end
 	end
-end
--- for each rect, choose a point and share its area
-areaForCell = {}
-for _,r in ipairs(solnRects) do
-	r.showPos = vec2(
-		math.random(r.bbox.min.x, r.bbox.max.x),
-		math.random(r.bbox.min.y, r.bbox.max.y)
-	)
-	r.area = (r.bbox:size()+1):product()
-	areaForCell[r.showPos.x] ??= {}
-	areaForCell[r.showPos.x][r.showPos.y] = r.area
+	-- for each rect, choose a point and share its area
+	areaForCell = {}
+	for i,r in ipairs(solnRects) do
+		r.color = i
+		r.showPos = vec2(
+			math.random(r.bbox.min.x, r.bbox.max.x),
+			math.random(r.bbox.min.y, r.bbox.max.y)
+		)
+		r.area = (r.bbox:size()+1):product()
+		areaForCell[r.showPos.x] ??= {}
+		areaForCell[r.showPos.x][r.showPos.y] = r.area
+	end
+
+	userRects = table()
+
+	-- state vars
+	youWonTime = nil
+	pressPos = nil
+	newRectColor = nil
+	nextColor = 0
 end
 
-userRects = table()
+newGame()
 
 update=||do
 	cls()
@@ -62,16 +111,16 @@ update=||do
 		end
 	end
 
+	-- show cheat boxes
+	if btn'a' then
+		for _,r in ipairs(solnRects) do
+			r:draw()
+		end
+	end
+
 	-- show user-created boxes
 	for i,r in ipairs(userRects) do
-		local b = r.bbox
-		local x = tileSize * b.min.x + 2
-		local y = tileSize * b.min.y + 2
-		local w = tileSize * (b.max.x - b.min.x + 1) - 6
-		local h = tileSize * (b.max.y - b.min.y + 1) - 6
-		rect(x, y, w, h, i)
-		rectb(x, y, w, h, 12)
-
+		r:draw()
 	end
 
 	-- show user-creating bbox
@@ -88,7 +137,7 @@ update=||do
 		local foundArea
 		newRectColor = nil
 		for _,r in ipairs(userRects) do
-			if r.bbox:touchesE(b) then
+			if r.bbox:touches(b) then
 				newRectColor = red
 				goto done
 			end
@@ -122,6 +171,14 @@ update=||do
 
 	text('X', mouseX - 4, mouseY - 4)
 
+	if youWonTime then
+		text('YOU WON', 20, 100, 12, -1, 5, 5)
+		if time() - youWonTime > 10 then
+			newGame()
+		end
+		return
+	end
+
 	if keyp'mouse_left' then
 		pressPos = mouseTilePos()
 		if not boardBBox:contains(pressPos) then
@@ -142,7 +199,20 @@ update=||do
 			local b = box2(pressPos):stretch(releasePos)
 			-- b's color is green if it touches one showPos and the areas match
 			-- b's color is red otherwise (and throw it away upon release?)
-			userRects:insert{bbox=b}
+			userRects:insert(Rect{bbox=b, color=nextColor+:=1})
+			local notAllCovered
+			for j=1,boardSize.y do
+				for i=1,boardSize.x do
+					if not userRects:find(nil, |r| r.bbox:contains(vec2(i,j))) then
+						notAllCovered = true
+						goto done2
+					end
+				end
+			end
+::done2::
+			if not notAllCovered then
+				youWonTime = time()
+			end
 		end
 		pressPos = nil
 		newRectColor = nil
