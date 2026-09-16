d16bde8085f8e2fbf56f536b78f0e2b10f3b205d
Christopher E. Moore <christopher.e.moore@gmail.com>
2026-09-16 15:08:15 +0800

    oops, bug in multiple tilemaps

---
+++ test-tilemaps/code.lua
@@ -0,0 +1,13 @@
+--[[
+draw tilemaps
+cycle with 'a' button
+--]]
+tilemapIndex = 0
+tilemapMax = 4
+update=||do
+	if btnp'a' or btnp'b' then tilemapIndex += 1 end
+	if btnp'x' or btnp'y' then tilemapIndex -= 1 end
+	tilemapIndex %= tilemapMax
+	tilemap(0,0, 32,32, 0,0, 0,false,0, tilemapIndex)
+	text('tilemap '..tilemapIndex, 0, 0)
+end
